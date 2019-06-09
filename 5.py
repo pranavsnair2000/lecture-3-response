2a0d5fadf5cb559f25e7dd0f3a9e01952f52c9f3
Pranav S Nair <pranavsnair2000@gmail.com>
2019-06-09 13:38:56 +0530

    Create 5.py
    create code for problem 5

---
+++ 5.py
@@ -0,0 +1,7 @@
+#5. Write a Python program to check if a string is numeric
+string=input("Enter string to check if it's numeric :   ")
+
+if(string.isnumeric()):
+    print("It is numeric")
+else:
+    print("not numeric")
